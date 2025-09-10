028eb999e0aa4584fe48b340a8c48225a241ae6a
Maksim <maksimnelder8@gmail.com>
2025-09-10 23:28:46 +0500

    Task1

---
+++ Tyuiu.NeldnerMK.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tyuiu.NeldnerMK.Sprint0.Task1.V0 {
+
+    class Program {
+
+        static void Main() {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
+
