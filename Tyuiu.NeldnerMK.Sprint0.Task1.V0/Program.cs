ceac20a8b2ac24b8a21446a2f8e129b4b916cddc
Maksim <maksimnelder8@gmail.com>
2025-09-10 23:55:06 +0500

    Task1

--- Tyuiu.NeldnerMK.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.NeldnerMK.Sprint0.Task1.V0/Program.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 
-namespace Tyuiu.NeldnerMK.Sprint0.Task1.V0 {
-
-    class Program {
-
-        static void Main() {
+namespace Tyuiu.NeldnerMK.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main()
+        {
             Console.WriteLine("Hello, World!");
+            string name = "Максим";
+            int age = 18;
+
+            Console.WriteLine($"Здравствуйте, меня зовут {name}, мне {age} лет");
             Console.ReadKey();
         }
     }
