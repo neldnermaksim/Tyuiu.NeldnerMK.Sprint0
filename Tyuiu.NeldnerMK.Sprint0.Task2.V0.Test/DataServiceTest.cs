c15dc9ca77e6d754a205cf20b187d23fce57210a
Maksim <maksimnelder8@gmail.com>
2025-09-12 00:54:16 +0500

    Task2

---
+++ Tyuiu.NeldnerMK.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.NeldnerMK.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.NeldnerMK.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Максим";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Максим", res);
+        }
+    }
+}
