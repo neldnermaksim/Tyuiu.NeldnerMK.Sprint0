7f94a2b118178f34441eb1e7f23f4ffb34f425cd
Maksim <maksimnelder8@gmail.com>
2025-09-13 23:09:03 +0500

    Task4

---
+++ Tyuiu.NeldnerMK.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.NeldnerMK.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.NeldnerMK.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(7, 7));
+            Console.WriteLine(DataService.Subtraction(10, 5));
+            Console.WriteLine(DataService.Multiplication(10, 5));
+            Console.WriteLine(DataService.Division(9, 3));
+            Console.ReadKey();
+        }
+    }
+}
