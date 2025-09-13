6dabca6badfbeaed10fe163526a0958e3dcb0a39
Maksim <maksimnelder8@gmail.com>
2025-09-14 00:26:00 +0500

    Task5

---
+++ Tyuiu.NeldnerMK.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.NeldnerMK.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.NeldnerMK.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(7, 7));
+            Console.WriteLine("A - B = " + DataService.Subtraction(10, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 5));
+            Console.WriteLine("A / B = " + DataService.Division(9, 3));
+            Console.ReadKey();
+        }
+    }
+}
